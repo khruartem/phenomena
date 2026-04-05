f4f622eeafd0075daf9bd52dd528fa983311fe4f
Artem Khrustalev <khru-artem@yandex.ru>
2026-04-06 01:15:20 +0300

    feat:подключил стили хэдера

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -2,9 +2,11 @@ import type { FC } from "react";
 
 import { CobrandUI } from "../cobrand";
 
+import styles from "./header.module.css";
+
 export const HeaderUI: FC = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <CobrandUI />
     </header>
   );
