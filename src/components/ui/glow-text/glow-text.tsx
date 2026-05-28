7c7f78da3e79d6eb7f8f02c19431635626ce42f0
Artem Khrustalev <khru-artem@yandex.ru>
2026-05-28 23:55:17 +0300

    feat:доработки glow-text в части подсветки двух фраз

--- src/components/ui/glow-text/glow-text.tsx
+++ src/components/ui/glow-text/glow-text.tsx
@@ -4,6 +4,9 @@ import type { TGlowTextUIProps } from "./types";
 
 import styles from "./glow-text.module.css";
 import clsx from "clsx";
+import { TextUI } from "../text";
+import { Typography } from "../../../utils/typography";
+import { Colors } from "../../../utils/colors";
 
 export const GlowTextUI: FC<TGlowTextUIProps> = ({ children }) => {
   const title = useRef<HTMLDivElement | null>(null);
@@ -60,94 +63,22 @@ export const GlowTextUI: FC<TGlowTextUIProps> = ({ children }) => {
       ref={scene}
     >
       <div className={styles.title} ref={title}>
-        <div
-          id="base"
-          className={clsx(
-            styles.title__base,
-            styles.title__base_imaged,
-          )}
-        >
-          <span>{children}</span>
-          <span>{children}</span>
+        <div id="base" className={clsx(styles.title__base)}>
+          <TextUI
+            typography={Typography.Text_KharkivTone_400_60}
+            color={Colors.Badge}
+          >
+            {"Институт внедрения навыков"}
+          </TextUI>
+          <span className={styles.title__base_imaged}>{children}</span>
         </div>
         <div id="lit" className={styles.title__lit} ref={lit}>
-          <span>{children}</span>
+          <TextUI typography={Typography.Text_KharkivTone_400_60}>
+            {"Институт внедрения навыков"}
+          </TextUI>
           <span>{children}</span>
         </div>
       </div>
     </div>
   );
 };
-
-// import { useRef, type FC } from "react";
-
-// import type { TGlowTextUIProps } from "./types";
-
-// import styles from "./glow-text.module.css";
-// import clsx from "clsx";
-
-// export const GlowTextUI: FC<TGlowTextUIProps> = ({ children }) => {
-//   const title = useRef<HTMLDivElement | null>(null);
-//   const lit = useRef<HTMLDivElement | null>(null);
-
-//   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-//     if (lit.current && title.current) {
-//       const litRect = lit.current?.getBoundingClientRect();
-//       const x = e.clientX - litRect?.left;
-//       const y = e.clientY - litRect?.top;
-//       lit.current.style.setProperty("--x", `${x}px`);
-//       lit.current.style.setProperty("--y", `${y}px`);
-//       title.current.style.setProperty("--x", `${x}px`);
-//       title.current.style.setProperty("--y", `${y}px`);
-//     } else return;
-//   };
-
-//   const handleMouseEnter = () => {
-//     const maskOn =
-//       "radial-gradient(circle 168px at var(--x) var(--y), black 0%, black 30%, transparent 100%)";
-//     const backgroundOn =
-//       "linear-gradient(180deg, var(--fire) 0%, var(--red) 100%)";
-
-//     if (lit.current && title.current) {
-//       lit.current.style.maskImage = maskOn;
-//       lit.current.style.backgroundImage = backgroundOn;
-//       lit.current.style.backgroundClip = "text";
-
-//       title.current.classList.add(styles.title_blured);
-//     } else return;
-//   };
-
-//   const handleMouseLeave = () => {
-//     const maskOff =
-//       "radial-gradient(circle 0px at -9999px -9999px, black 0%, transparent 100%)";
-//     const backgroundOff = "unset";
-
-//     if (lit.current && title.current) {
-//       lit.current.style.maskImage = maskOff;
-//       lit.current.style.backgroundImage = backgroundOff;
-//       lit.current.style.backgroundClip = "unset";
-
-//       title.current.classList.remove(styles.title_blured);
-//     } else return;
-//   };
-
-//   return (
-//     <div
-//       className={styles.title}
-//       ref={title}
-//       onMouseMove={handleMouseMove}
-//       onMouseEnter={handleMouseEnter}
-//       onMouseLeave={handleMouseLeave}
-//     >
-//       <div
-//         id="base"
-//         className={clsx(styles.title__base, styles.title__base_imaged)}
-//       >
-//         {children}
-//       </div>
-//       <div id="lit" className={styles.title__lit} ref={lit}>
-//         {children}
-//       </div>
-//     </div>
-//   );
-// };
