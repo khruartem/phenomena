6c2cbf8d1b03fda9cd51f8c4d382f58dff25df1c
Artem Khrustalev <khru-artem@yandex.ru>
2026-06-03 00:12:12 +0300

    refactoring:начал переписывать хуки useStyle с использованием фабрики для хуков useStyle

---
+++ src/components/ui/committeeman/useStyle.ts
@@ -0,0 +1,13 @@
+import { createUseStyleHook } from "../../../utils/createUseStyleHook";
+
+export const useStyle = createUseStyleHook({
+  width: {
+    large: 520,
+    desktop: 368,
+    laptop: 460,
+  },
+  height: {
+    large: 228,
+    desktop: 268,
+  },
+});
